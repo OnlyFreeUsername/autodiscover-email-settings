6d54bb709f2c4177b31f5275f5d6104854222a4f
Michael Chen <git@cnml.de>
2024-12-11 13:31:16 +0100

    feat: build script and labels

--- build.py
+++ build.py
@@ -1,40 +1,69 @@
-#!python
-import subprocess
-import argparse
-from itertools import chain
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from datetime import datetime, timezone
+from pathlib import Path
+from subprocess import run
 
-dockercmd = 'docker'
+DOCKER_CMD = "docker"
 
-parser = argparse.ArgumentParser(description='Create custom images.')
-parser.add_argument('tags',
-                    metavar='TAG',
-                    nargs='+',
-                    help='Version tags to build.')
+parser = ArgumentParser(description="Create custom images.")
+parser.add_argument("tags", metavar="TAG", nargs="+", help="Version tags to build.")
 
 args = parser.parse_args()
 
 platforms = [
-    'linux/amd64', 'linux/arm64', 'linux/ppc64le', 'linux/s390x',
-    'linux/arm/v7', 'linux/arm/v6'
+    "linux/amd64",
+    "linux/arm64/v8",
+    "linux/s390x",
+    "linux/arm/v7",
+    "linux/arm/v6",
 ]
 
 
-def build(image, directory, platforms, build_args=None):
+def build(
+    image: str,
+    directory: Path,
+    platforms: list[str],
+    build_args: list[tuple[str, str]] | None = None,
+):
     if build_args is None:
         build_args = []
-    build_args = list(
-        chain.from_iterable(['--build-arg', f'{arg}={val}']
-                            for (arg, val) in build_args))
-    platformlist = ','.join(platforms)
-    subprocess.run([
-        dockercmd, 'buildx', 'build', '-f', 'Dockerfile', '--platform',
-        platformlist, '-t', image
-    ] + build_args + ['--push', directory],
-                   check=True)
+    platformlist = ",".join(platforms)
+    CMD = [
+        DOCKER_CMD,
+        "buildx",
+        "build",
+        "-f",
+        "Dockerfile",
+        "--push",
+        "--pull",
+        "--platform",
+        platformlist,
+        "-t",
+        image,
+    ]
+    for arg, val in build_args:
+        CMD.extend(["--build-arg", f"{arg}={val}"])
+    CMD.append(str(directory))
+    run(CMD, check=True)
 
 
-for tag in args.tags:
-    targetimage = f'chenio/autodiscover-email-settings:{tag}'
+def main():
+    for tag in args.tags:
+        targetimage = f"chenio/autodiscover-email-settings:{tag}"
 
-    print(f'Building image {targetimage}')
-    build(targetimage, '.', platforms, [('TAG', tag)])
+        print(f"Building image {targetimage}")
+        build(
+            targetimage,
+            Path.cwd(),
+            platforms,
+            [
+                ("TAG", tag),
+                ("BUILD_DATE", datetime.now(timezone.utc).isoformat()),
+                ("VCS_REF", tag),
+            ],
+        )
+
+if __name__ == "__main__":
+    main()
+
